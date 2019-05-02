098a55161c302022affedd16a3b685419dea44bd
Igor Santos Peretta <iperetta@gmail.com>
2019-05-02 07:47:58 -0300

    js002

--- resp.js
+++ resp.js
@@ -3,3 +3,8 @@ console.log(123+5.6);
 console.log(999%56)
 console.log('Olá mundo!')
 console.log('Abrerração às pressas çõôüã!')
+
+function Q() {
+    resp = 42;
+    document.getElementById('R').innerHTML = "Texto da resposta: " + resp;
+}
